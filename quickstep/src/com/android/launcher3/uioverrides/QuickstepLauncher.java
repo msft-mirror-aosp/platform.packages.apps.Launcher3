0a65c5bd72d755856148faeb070a5d2ad2c79cb0
Jagrut Desai <jagrutdesai@google.com>
2024-10-07 15:16:43 -0700

    Fix Taskbar to Hotseat Animation for Non-Predective back apps
    - Refactored Taskbar animation duration to under one method and added logic to sync animation duration according to enableScalingRevealHomeAnimation.
    - Change the interpolator for TaskbarLauncherStateController based on enableScalingRevealHomeAnimation.
    
    Test: Presubmit, Manual
    Bug: 369378541
    Flag: EXEMPT Bug Fix
    Change-Id: I942829b475f91e9102801b758eb7ee0d27eee912

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -1315,6 +1315,10 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
         mTISBindHelper.setPredictiveBackToHomeInProgress(isInProgress);
     }
 
+    public boolean getPredictiveBackToHomeInProgress() {
+        return mIsPredictiveBackToHomeInProgress;
+    }
+
     @Override
     public boolean areDesktopTasksVisible() {
         DesktopVisibilityController desktopVisibilityController = getDesktopVisibilityController();
