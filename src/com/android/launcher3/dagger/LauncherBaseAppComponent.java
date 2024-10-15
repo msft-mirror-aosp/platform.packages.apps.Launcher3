68f5a205f47de1320bb62079f32d71208eca45a6
Charlie Anderson <charlander@google.com>
2024-10-15 15:39:33 -0400

    Convert ItemInstallQueue to use Dagger
    Bug: b/372012340
    Test: Presubmit
    Flag: EXEMPT Dagger change
    Change-Id: I28033f7f164d52cf6d69035b2b652fbf8eb2b393

--- src/com/android/launcher3/dagger/LauncherBaseAppComponent.java
+++ src/com/android/launcher3/dagger/LauncherBaseAppComponent.java
@@ -19,6 +19,7 @@ package com.android.launcher3.dagger;
 import android.content.Context;
 
 import com.android.launcher3.graphics.IconShape;
+import com.android.launcher3.model.ItemInstallQueue;
 import com.android.launcher3.pm.InstallSessionHelper;
 import com.android.launcher3.util.ApiWrapper;
 import com.android.launcher3.util.DaggerSingletonTracker;
@@ -45,6 +46,7 @@ public interface LauncherBaseAppComponent {
     CustomWidgetManager getCustomWidgetManager();
     IconShape getIconShape();
     InstallSessionHelper getInstallSessionHelper();
+    ItemInstallQueue getItemInstallQueue();
     RefreshRateTracker getRefreshRateTracker();
     ScreenOnTracker getScreenOnTracker();
     SettingsCache getSettingsCache();
