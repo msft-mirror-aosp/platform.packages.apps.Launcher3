8f9c2cc0209e90e8539211c5bb0ac3aeebd773ca
Liran Binyamin <liranb@google.com>
2024-11-08 14:26:31 -0500

    Animate the bubble notification in overview
    Flag: com.android.wm.shell.enable_bubble_bar
    Fixes: 378095516
    Test: manual
           - swipe to overview
           - send bubble notification
           - observe bubble animation
    Change-Id: I9e52f1f7c7ddcc19b075af662d79ee9327b89fdb

--- quickstep/src/com/android/launcher3/taskbar/bubbles/BubbleBarViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/bubbles/BubbleBarViewController.java
@@ -893,9 +893,10 @@ public class BubbleBarViewController {
             mBubbleBarViewAnimator.animateToInitialState(bubble, isInApp, isExpanding);
             return;
         }
-        boolean persistentTaskbarOrOnHome = mBubbleStashController.isBubblesShowingOnHome()
+        // if we're not stashed or we're in persistent taskbar, animate for collapsed state.
+        boolean animateForCollapsed = !mBubbleStashController.isStashed()
                 || !mBubbleStashController.isTransientTaskBar();
-        if (persistentTaskbarOrOnHome) {
+        if (animateForCollapsed) {
             mBubbleBarViewAnimator.animateBubbleBarForCollapsed(bubble, isExpanding);
             return;
         }
